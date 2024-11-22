8fb63104a92c7e33bb0c3904a184c3d8b92fe555
Kalyan Mudumby <theinhumaneme@gmail.com>
2024-11-22 23:54:44 +0530

    init main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod objects;
+mod utils;
+use std::fs;
+
+use objects::structs::NodeConfig;
+use utils::error::ConfigError;
+
+use crate::objects::structs::Validate;
+
+fn load_config(file_path: &str) -> Result<NodeConfig, ConfigError> {
+    let contents = fs::read_to_string(file_path)?;
+    let config: NodeConfig = serde_json::from_str(&contents)?;
+    config.validate()?;
+    Ok(config)
+}
 fn main() {
-    println!("New Project Begins")
+    match load_config("test.json") {
+        Ok(config) => {
+            println!("Configuration loaded successfully: {:#?}", config);
+        }
+        Err(e) => {
+            eprintln!("Error loading configuration: {}", e);
+        }
+    }
 }
