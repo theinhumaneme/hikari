9c5bc9ff6a05a7c624e10f2fc2b3e02ba7e6f600
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-06-01 03:20:02 +0530

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,48 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
+  pkgs = import nixpkgs {
+    config = {
+      unfree = false;
+    };
+    overlays = [
+      (import (
+        builtins.fetchTarball {
+          url = "https://github.com/oxalica/rust-overlay/tarball/master";
+        }
+      ))
+    ];
+  };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    # Pull in the latest nightly Rust (with rust-src, rustfmt, and clippy)
+    (rust-bin.selectLatestNightlyWith (
+      toolchain:
+      toolchain.default.override {
+        extensions = [
+          "rust-src"
+          "rustfmt"
+          "clippy"
+        ];
+      }
+    ))
+
+    # Keep the rest of your tools as before
+    rust-analyzer
+    cargo-bump
+    sqlx-cli
+
+    lolcat
+    cowsay
+    docker
+    docker-buildx
+    docker-compose
+  ];
+
+  GREETING = "Hikari Development Environment Activated!";
+
+  shellHook = ''
+    echo $GREETING | cowsay | lolcat
+  '';
+}
